fe8d6cbb3aecb9497d45984147ca04dc43196fb5
Chris N <swordsman9613@gmail.com>
2016-02-03 18:23:00 -1000

    Fix doubled code
    Stupid boy is stupid.

--- SpellCheckerv1.sh
+++ SpellCheckerv1.sh
@@ -24,36 +24,6 @@ done < $fname
 for word in ${words[@]}
 do
     if ! grep $word "CABSC.dict" > /dev/null
-    then 
-        echo $word is not in dictionary
-"spellcheckerv1.sh" 41 lines, 744 characters written
-cs350s1@cs350admin-Virtual-Machine ~ $ cat spellcheckerv1.sh
-#!/bin/bash
-echo "Enter name of file to check:"
-read fname
-
-#Make array to hold words
-words=()
-
-#Check if dictionary file exists yet
-if [ ! -f CABSC.dict ]
-then
-    echo "" > CABSC.dict
-fi
-
-#Read file line by line
-while read line
-do
-    #Read each word on line
-    for word in $line
-    do
-        words+=("$word")
-    done
-done < $fname
-
-for word in ${words[@]}
-do
-    if ! grep $word "CABSC.dict" > /dev/null
     then
         echo $word is not in dictionary
         echo "Would you like to add it? <y/n>"
